239dd70a9e7b0a6a6b9958bceea0c57c9784be57
martaxxa <mchacartegui29@gmail.com>
2024-12-12 20:24:23 +0100

    Add check

--- Adatask/js/main.js
+++ Adatask/js/main.js
@@ -1,2 +1,16 @@
 'use strict'
 
+const add = document.querySelector('.js_add');
+const btnAdd = document.querySelector('.btn-add');
+const listTask = document.querySelector('.task-ul');
+const check = document.querySelector ('.checkbox');
+
+btnAdd.addEventListener ('click' , (ev) => {
+  ev.preventDefault();
+  listTask.innerHTML += `<li><input class="checkbox" type="checkbox" id="checkBox">` + ' ' + `${add.value}</li>`;
+  add.value = " ";
+});
+
+check.addEventListener ('click' , (ev) => {
+  
+});
