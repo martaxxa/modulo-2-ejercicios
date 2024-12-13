fbb6c7390083fb38b98909cea54deceb6e86c42e
martaxxa <mchacartegui29@gmail.com>
2024-12-13 13:10:40 +0100

    Add cambios en estilos

--- Adatask/js/main.js
+++ Adatask/js/main.js
@@ -5,7 +5,39 @@ const btnAdd = document.querySelector('.btn-add');
 const listTask = document.querySelector('.task-ul');
 const check = document.querySelector ('.checkbox');
 
-btnAdd.addEventListener ('click' , (ev) => {
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+for (const addObj of tasks) {
+    
+  if(addObj.completed === true){  
+      listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox" checked>` + ' ' + `${addObj.name}</li>`;
+  }
+  else{
+      listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox">` + ' ' + `${addObj.name}</li>`;
+  }
+};
+
+btnAdd.addEventListener('click' , (ev) => {
+  ev.preventDefault();
+  listTask.innerHTML += `<li><input class="checkbox"  type="checkbox" id="checkBox">` + ' ' + `${add.value}</li>`;
+  add.value = " ";
+});
+
+
+
+
+
+
+/*btnAdd.addEventListener ('click' , (ev) => {
   ev.preventDefault();
   listTask.innerHTML += `<li><input class="checkbox" type="checkbox" id="checkBox">` + ' ' + `${add.value}</li>`;
   add.value = " ";
@@ -13,4 +45,4 @@ btnAdd.addEventListener ('click' , (ev) => {
 
 check.addEventListener ('click' , (ev) => {
   
-});
+});*/
